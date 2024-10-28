10bacce20a194e635ec1b2f680ea97ceb9b83cf8
Rajesh-patale <rajeshpatale888@gmail.com>
2024-10-28 15:24:25 +0530

    new Project

---
+++ src/main/java/com/openfuture/OpenfutureApplication.java
@@ -0,0 +1,14 @@
+package com.openfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfutureApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OpenfutureApplication.class, args);
+	}
+
+}
