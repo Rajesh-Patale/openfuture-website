3391a4bdfff34de501600cdb7d6d8064739d29df
Anurag26091994 <112397456+Anurag26091994@users.noreply.github.com>
2024-10-29 14:02:47 +0530

    Add Admin Functionality

---
+++ src/main/java/com/openfuture/Exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.openfuture.Exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
